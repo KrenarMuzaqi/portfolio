5d264ef847e7fbf25ad02a06e395ed6850e31397
Krenar <krenarmuzaqi@gmail.com>
2020-04-21 23:06:57 +0200

    Updates

---
+++ precache-manifest.4326396e5bffae63f8bd2dfbc25fed2a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7563302357c500b1c13589f967c80563",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6c6588446850b252e62a",
+    "url": "/portfolio/static/css/main.dd170f0c.chunk.css"
+  },
+  {
+    "revision": "b0da3ad8b0fb7fb920c0",
+    "url": "/portfolio/static/js/2.9ef553a1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.9ef553a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6588446850b252e62a",
+    "url": "/portfolio/static/js/main.1440d8cb.chunk.js"
+  },
+  {
+    "revision": "5ac0b2b5146ed69ded33",
+    "url": "/portfolio/static/js/runtime-main.9612f1a5.js"
+  },
+  {
+    "revision": "035cce00ad53f08df0b73c7e1f2ba26f",
+    "url": "/portfolio/static/media/about-img.035cce00.jpg"
+  },
+  {
+    "revision": "4a6087c78e433eb47748752ce402475d",
+    "url": "/portfolio/static/media/carton-grey.4a6087c7.svg"
+  },
+  {
+    "revision": "2c0cb8992718615805bce6e78d19335f",
+    "url": "/portfolio/static/media/email-development.2c0cb899.svg"
+  },
+  {
+    "revision": "cef8d2fe0da926c97cd15001fce79476",
+    "url": "/portfolio/static/media/email-icon.cef8d2fe.svg"
+  },
+  {
+    "revision": "ab92ad6d992f203adfc29fddf0efd3e6",
+    "url": "/portfolio/static/media/facebook-icon.ab92ad6d.svg"
+  },
+  {
+    "revision": "09c665963f6ea20999a955b3b85e0c5f",
+    "url": "/portfolio/static/media/instagram-icon.09c66596.svg"
+  },
+  {
+    "revision": "380d97a671b3d01b218edddf720b4611",
+    "url": "/portfolio/static/media/linkedin-icon.380d97a6.svg"
+  },
+  {
+    "revision": "5fd88e3a8eac8dac442825fa730d664c",
+    "url": "/portfolio/static/media/mkblack.5fd88e3a.svg"
+  },
+  {
+    "revision": "2fc35be6398e75aee200a189d6639b02",
+    "url": "/portfolio/static/media/mkwhite.2fc35be6.svg"
+  },
+  {
+    "revision": "99e9d0f4617f92dbde39f61dfa247c30",
+    "url": "/portfolio/static/media/responsive-design.99e9d0f4.svg"
+  },
+  {
+    "revision": "98d7177864d0b091be4ffcec5f2ba3a2",
+    "url": "/portfolio/static/media/scrollArrow.98d71778.svg"
+  },
+  {
+    "revision": "f1841151eb25fb31c63713d3e62b6537",
+    "url": "/portfolio/static/media/support.f1841151.svg"
+  },
+  {
+    "revision": "88c38bdc6f8a183f915ded830d225ef3",
+    "url": "/portfolio/static/media/twitter-icon.88c38bdc.svg"
+  },
+  {
+    "revision": "9d30345fb9082da65ff580bf3f358d3e",
+    "url": "/portfolio/static/media/web-development.9d30345f.svg"
+  },
+  {
+    "revision": "60250cede0ce001d5a932dd4f7bc09c0",
+    "url": "/portfolio/static/media/working-portfolio.60250ced.svg"
+  }
+]);
