27e9300b127de48c22eced395c00585dbdaeb292
Krenar <krenarmuzaqi@gmail.com>
2020-09-29 16:39:10 +0200

    Feat: Projects added on portfolio section

--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -3,6 +3,13 @@ import React, { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
+import MusicAppImg from "../../assets/images/music-app-mockup.png";
+import LibertyImg from "../../assets/images/statue-of-liberty--mockup.png";
+import FitnessPrimeImg from "../../assets/images/fitness-prime.png";
+import SecureVpnImg from "../../assets/images/secure-vpn-mockup.png";
+import Movement4 from "../../assets/images/movement4-mockup.png";
+import Fylo from "../../assets/images/fylo-mockup.png";
+
 import "./_portfolio.scss";
 
 const Portfolio = () => {
@@ -17,16 +24,38 @@ const Portfolio = () => {
         </h1>
 
         <div className="portfolio-boxs">
-          <div className="portfolio--box" data-aos="fade-right">
-            <div className="portfolio-img fylo-img"></div>
-            <div className="project-title">
-              <h1>Fylo Landing Page</h1>
-              <span>HTML/SCSS</span>
+          <div className="portfolio--box" data-aos="fade-left">
+            <div className="portfolio-img movement4_img">
+              <img src={Movement4} alt="Movement4 img" />
             </div>
-            <p className="project-description">My project</p>
+            <div className="project-title">
+              <h1>MOVEMENT4</h1>
+              <span>HTML / SCSS / JS</span>
+            </div>
+            <p className="project-description">Upwork Project</p>
             <div className="project-btn">
               <a
-                href="https://coding-tips.github.io/Fylo/"
+                href="https://www.movement4.com.au/"
+                target="_blank"
+                className="view-btn "
+              >
+                View
+              </a>
+            </div>
+          </div>
+
+          <div className="portfolio--box" data-aos="fade-right">
+            <div className="portfolio-img fylo-img">
+              <img src={LibertyImg} alt="" />
+            </div>
+            <div className="project-title">
+              <h1>Statue of Liberty Tour</h1>
+              <span>HTML / SCSS / JS</span>
+            </div>
+            <p className="project-description">Upwork Project</p>
+            <div className="project-btn">
+              <a
+                href="https://statue-of-liberty-tour.netlify.app/"
                 target="_blank"
                 className="view-btn"
               >
@@ -36,17 +65,19 @@ const Portfolio = () => {
           </div>
 
           <div className="portfolio--box" data-aos="fade-left">
-            <div className="portfolio-img budget-app_img"></div>
-            <div className="project-title">
-              <h1>Budget App</h1>
-              <span>React/HTML/SCSS</span>
+            <div className="portfolio-img budget-app_img">
+              <img src={MusicAppImg} alt="music app img" />
             </div>
-            <p className="project-description">My project</p>
+            <div className="project-title">
+              <h1>Music App</h1>
+              <span>HTML / SCSS / JS</span>
+            </div>
+            <p className="project-description">Upwork Project</p>
             <div className="project-btn">
               <a
-                href="https://km-budget-app.netlify.app/"
+                href="https://music-app-km.netlify.app/"
                 target="_blank"
-                className="view-btn"
+                className="view-btn "
               >
                 View
               </a>
@@ -54,17 +85,19 @@ const Portfolio = () => {
           </div>
 
           <div className="portfolio--box" data-aos="fade-right">
-            <div className="portfolio-img todo-list_img"></div>
-            <div className="project-title">
-              <h1>Todo List</h1>
-              <span>React/HTML/SCSS</span>
+            <div className="portfolio-img ">
+              <img src={FitnessPrimeImg} alt="Fitness Prime img" />
             </div>
-            <p className="project-description">My project</p>
+            <div className="project-title">
+              <h1>Fitness Prime - Platform</h1>
+              <span>HTML / SCSS / JS</span>
+            </div>
+            <p className="project-description">Upwork Project</p>
             <div className="project-btn">
               <a
-                href="https://todo-list-km.netlify.app/"
+                href="https://fitness-prime.netlify.app/"
                 target="_blank"
-                className="view-btn"
+                className="view-btn "
               >
                 View
               </a>
@@ -72,15 +105,38 @@ const Portfolio = () => {
           </div>
 
           <div className="portfolio--box" data-aos="fade-left">
-            <div className="portfolio-img contact-manager_img"></div>
-            <div className="project-title">
-              <h1>Contact Manager</h1>
-              <span>React/HTML/BOOTSTRAP</span>
+            <div className="portfolio-img fitness-prime_img">
+              <img src={SecureVpnImg} alt="Secure VPN img" />
             </div>
-            <p className="project-description">My project</p>
+            <div className="project-title">
+              <h1>Secure VPN</h1>
+              <span>HTML / SCSS / Bootstrap</span>
+            </div>
+            <p className="project-description">Upwork Project</p>
+
             <div className="project-btn">
               <a
-                href="https://contact-manager-km.netlify.app/#/"
+                href="https://secure-vpn.netlify.app/"
+                target="_blank"
+                className="view-btn"
+              >
+                View
+              </a>
+            </div>
+          </div>
+          <div className="portfolio--box" data-aos="fade-right">
+            <div className="portfolio-img fitness-prime_img">
+              <img src={Fylo} alt="Fylo img" />
+            </div>
+            <div className="project-title">
+              <h1>Fylo</h1>
+              <span>HTML / SCSS</span>
+            </div>
+            <p className="project-description">Project</p>
+
+            <div className="project-btn">
+              <a
+                href="https://fylo-km.netlify.app/"
                 target="_blank"
                 className="view-btn"
               >
